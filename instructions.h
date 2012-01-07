8f8f7e098b23fbd0156b326a13c4b3987b60f6b1
Christian Zeller <chrizel@gmail.com>
2012-01-07 18:32:21 +0100

    SBC<byte> uses new arithmetic functions

--- instructions.h
+++ instructions.h
@@ -327,14 +327,12 @@ struct SBC_Instruction : public ReferenceInstruction<T> {
 template <>
 struct SBC_Instruction<byte> : public ReferenceInstruction<byte> {
     void run() {
-        byte a = ref0->get();
-        byte n = ref1->get();
-        byte x = a - (n + (cpu->flagC() ? 1 : 0));
-        ref0->set(x);
-        cpu->flagZ(x == 0);
+        flags f;
+        ref0->set(subByte(ref0->get(), ref1->get() + (cpu->flagC() ? 1 : 0), f)); //TODO
+        cpu->flagZ(f.z);
         cpu->flagN(1);
-        cpu->flagH(0); // TODO: half carry flag
-        cpu->flagC(x <= a); // TODO: carry flag
+        cpu->flagH(f.h);
+        cpu->flagC(f.c);
         cpu->pc += length-1;
     }
 };
